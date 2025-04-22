f7d0ee023b074b8693f7e86393b9078be94bde5f
Alex Joyce <150167187+atos-ajoyce@users.noreply.github.com>
2025-04-22 15:37:08 +0100

    Merge updated façade to main (#13)
    * Created skeleton facade solution (#3)
    
    * update project name and add docker file (#5)
    
    * Feature/prn rep exp facade pipeline (#4) (#6)
    
    * Pipeline file for prn-rep-ex-facade
    
    * Amend the project and test file
    
    Co-authored-by: Himat Varsani <156190828+himatvarsani-atos@users.noreply.github.com>
    
    * Updtae unit test projects with SQ exclusions and conerlet itemgroups
    
    * Change nugetTasks to true
    
    * Update folders and references
    
    * Add sonarqube exclusions
    
    * SQ excusion for program.cs
    
    * Dockerfile location fix
    
    * explicit program.cs ignore
    
    * sonarqube ignores
    
    * Fix typo in ALL projects
    
    * Typos in docker file
    
    * More typos in settings
    
    * Update project name to fix typo and naming
    
    * explicit ignore on top level program.cs
    
    * additional ignore
    
    * ignores fo SQ
    
    * add directory build properties file
    
    * Fix dockerfile line 20 for build failure
    
    * Update docker file
    
    * Change Artifact feed to defra-common
    
    * Delete duplicate line
    
    * replicating settings from frontend dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * remove SQ error for root user (#11)
    
    * Saffoora nm patch 1 update for sq proj updates (#12)
    
    * remove SQ error for root user
    
    * update proj files with SQ exclusions
    
    ---------
    
    Co-authored-by: johnfitzgeraldEviden <john.fitzgerald.external@eviden.com>
    Co-authored-by: Saffoora Mohammed <saffoora.mohammed@eviden.com>
    Co-authored-by: Himat Varsani <156190828+himatvarsani-atos@users.noreply.github.com>

---
+++ src/Epr.Reprocessor.Exporter.Facade.App/Config/AccountsServiceApiConfig.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Epr.Reprocessor.Exporter.Facade.App.Config;
+
+[ExcludeFromCodeCoverage]
+public class AccountsServiceApiConfig
+{
+    public const string SectionName = "AccountsServiceApiConfig";
+
+    public string BaseUrl { get; set; } = null!;
+    public string AccountServiceClientId { get; set; } = null!;
+    public string Certificate { get; set; } = null!;
+    public int Timeout { get; set; }
+    public int ServicePooledConnectionLifetime { get; set; }
+    public int ServiceRetryCount { get; set; }
+    public AccountsServiceEndpoints Endpoints { get; set; } = null!;
+}
+
+[ExcludeFromCodeCoverage]
+public class AccountsServiceEndpoints
+{
+    public string PendingApplications { get; set; } = null!;
+    public string GetOrganisationsApplications { get; set; } = null!;
+    public string ManageEnrolment { get; set; } = null!;
+    public string TransferOrganisationNation { get; set; } = null!;
+    public string UserOrganisations { get; set; } = null!;
+    public string CreateRegulator { get; set; } = null!;
+    public string GetRegulator { get; set; } = null!;
+    public string RegulatorInvitation { get; set; } = null!;
+    public string RegulatorEnrollment { get; set; } = null!;
+    public string RegulatorInvitedUser { get; set; } = null!;
+    public string GetRegulatorUsers { get; set; } = null!;
+    public string GetOrganisationsBySearchTerm { get; set; } = null!;
+    public string GetUsersByOrganisationExternalId { get; set; } = null!;
+    public string GetOrganisationDetails { get; set; } = null!;
+    public string RegulatorRemoveApprovedUser { get; set; } = null!;
+    
+    public string AddRemoveApprovedUser { get; set; } = null!;
+}
---
+++ src/Epr.Reprocessor.Exporter.Facade.App/Config/AntivirusApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Epr.Reprocessor.Exporter.Facade.App.Config;
+
+[ExcludeFromCodeCoverage]
+public class AntivirusApiConfig
+{
+    public const string SectionName = "AntivirusApi";
+
+    public string BaseUrl { get; set; }
+
+    public string SubscriptionKey { get; set; }
+
+    public string TenantId { get; set; }
+
+    public string ClientId { get; set; }
+
+    public string ClientSecret { get; set; }
+
+    public string Scope { get; set; }
+
+    public int Timeout { get; set; }
+
+    public bool EnableDirectAccess { get; set; } = false;
+
+    public string CollectionSuffix { get; set; }
+
+    public bool PersistFile { get; set; }
+}
+
