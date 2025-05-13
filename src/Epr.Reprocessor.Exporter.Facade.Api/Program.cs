a29015b1a1e61827773319e51784fde00beac8da
Alex Joyce <alex.joyce.external@eviden.com>
2025-05-13 11:11:07 +0100

    stylecop changes - remove commented code

--- src/Epr.Reprocessor.Exporter.Facade.Api/Program.cs
+++ src/Epr.Reprocessor.Exporter.Facade.Api/Program.cs
@@ -12,55 +12,60 @@ namespace Epr.Reprocessor.Exporter.Facade.Api;
 [ExcludeFromCodeCoverage]
 public class Program
 {
-	private static async Task Main(string[] args)
-	{
-		var builder = WebApplication.CreateBuilder(args);
-		builder.Services
-			.AddApplicationInsightsTelemetry()
-			.AddHealthChecks();
-		// Logging
-		builder.Services.AddLogging();
-		// Authentication
-		builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-			.AddMicrosoftIdentityWebApi(options =>
-			{
-				builder.Configuration.Bind("AzureAdB2C", options);
-			}, options =>
-			{
-				builder.Configuration.Bind("AzureAdB2C", options);
-			});
-		// Authorization
-		var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
-		builder.Services.AddAuthorizationBuilder().AddPolicy("AuthUser", policy);
-		// General Config
-		builder.Services.AddControllers()
-			.AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
-		builder.Services.AddEndpointsApiExplorer();
-		builder.Services.AddSwaggerGen(options =>
-		{
-			options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-			{
-				Type = SecuritySchemeType.Http,
-				Scheme = "Bearer"
-			});
-			options.OperationFilter<AddAuthHeaderOperationFilter>();
-		});
-		// App
-		var app = builder.Build();
-		app.UseExceptionHandler(app.Environment.IsDevelopment() ? "/error-development" : "/error");
-		app.UseSwagger();
-		app.UseSwaggerUI();
-		if (app.Environment.IsDevelopment())
-		{
-			IdentityModelEventSource.ShowPII = true;
-		}
-		app.UseHttpsRedirection();
-		app.UseAuthentication();
-		app.UseAuthorization();
-		app.MapControllers();
-		//app.MapHealthChecks(
-		//    builder.Configuration.GetValue<string>("HealthCheckPath"),
-		//    HealthCheckOptionBuilder.Build()).AllowAnonymous();
-		await app.RunAsync();
-	}
+    private static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services
+            .AddApplicationInsightsTelemetry()
+            .AddHealthChecks();
+
+        // Logging
+        builder.Services.AddLogging();
+
+        // Authentication
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddMicrosoftIdentityWebApi(options =>
+            {
+                builder.Configuration.Bind("AzureAdB2C", options);
+            }, options =>
+            {
+                builder.Configuration.Bind("AzureAdB2C", options);
+            });
+
+        // Authorization
+        var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+        builder.Services.AddAuthorizationBuilder().AddPolicy("AuthUser", policy);
+
+        // General Config
+        builder.Services.AddControllers()
+            .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.Http,
+                Scheme = "Bearer"
+            });
+            options.OperationFilter<AddAuthHeaderOperationFilter>();
+        });
+
+        // App
+        var app = builder.Build();
+        app.UseExceptionHandler(app.Environment.IsDevelopment() ? "/error-development" : "/error");
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        if (app.Environment.IsDevelopment())
+        {
+            IdentityModelEventSource.ShowPII = true;
+        }
+        app.UseHttpsRedirection();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.MapControllers();
+
+        await app.RunAsync();
+    }
 }
