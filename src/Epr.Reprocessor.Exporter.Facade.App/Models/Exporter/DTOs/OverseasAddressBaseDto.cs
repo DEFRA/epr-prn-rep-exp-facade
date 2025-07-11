a9643b9ca545a013efff4d4327fb855450552d8f
Bhaarath Balraj <bhaarath.balrak@Defra.onmicrosoft.com>
2025-07-11 14:45:32 +0100

    Addressed PR comments

--- src/Epr.Reprocessor.Exporter.Facade.App/Models/Exporter/DTOs/OverseasAddressBaseDto.cs
+++ src/Epr.Reprocessor.Exporter.Facade.App/Models/Exporter/DTOs/OverseasAddressBaseDto.cs
@@ -12,8 +12,10 @@ namespace Epr.Reprocessor.Exporter.Facade.App.Models.Exporter.DTOs
         public required string AddressLine2 { get; set; }
         [MaxLength(70)]
         public required string CityorTown { get; set; }
+        [MaxLength(100)]
         public required string Country { get; set; }
         public Guid Id { get; set; }
+        [MaxLength(100)]
         public required string OrganisationName { get; set; }
         [MaxLength(20)]
         public required string PostCode { get; set; }
