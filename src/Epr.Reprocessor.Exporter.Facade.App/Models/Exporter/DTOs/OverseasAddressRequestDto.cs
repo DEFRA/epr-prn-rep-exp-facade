39d4ced22ffd22bfa5789f5f3103ca868c1b36e5
Adeel Dar (ext) <adeel.dar.external@eviden.com>
2025-07-16 11:36:33 +0100

    Fixed the overseas address issue

--- src/Epr.Reprocessor.Exporter.Facade.App/Models/Exporter/DTOs/OverseasAddressRequestDto.cs
+++ src/Epr.Reprocessor.Exporter.Facade.App/Models/Exporter/DTOs/OverseasAddressRequestDto.cs
@@ -26,13 +26,13 @@ public record OverseasAddressRequestDto(
             address.CountryName,
             address.SiteCoordinates,
             createdBy,
-            address.OverseasAddressContact.Select(contact => new OverseasAddressContactDto(
+            address.OverseasAddressContacts.Select(contact => new OverseasAddressContactDto(
                 contact.FullName,
                 contact.Email,
                 contact.PhoneNumber,
                 createdBy)).ToList(),
             address.OverseasAddressWasteCodes.Select(wasteCode => new OverseasAddressWasteCodesDto(
-                wasteCode.Id,
+                wasteCode.ExternalId,
                 wasteCode.CodeName)).ToList()
         )).ToList();
 
